50fb5bf1ca786f34b5cc97f5232b67d154ec077c
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2021-03-09 10:56:52 +0100

    l

---
+++ Genriskaklasser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.AccessControl;
+using System;
+using System.Collections.Generic;
+
+
+namespace Genriskaklasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creature<string> c = new Creature<string>();
+            // Creature<int> c2 = new Creature<int>();
+            // //instans<Typen av T> c3 = new Creature<Typen av T>();
+
+            // c.category = "6";
+            // c2.category = 6;
+            
+            Queue<String> orders = new Queue<string>();
+
+            orders.Enqueue("walk");
+            orders.Enqueue("stand");
+            orders.Enqueue("patrol");
+
+            string order = orders.Dequeue();
+            
+            Console.WriteLine(orders);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
